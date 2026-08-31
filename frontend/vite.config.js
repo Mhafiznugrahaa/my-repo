7dda2e4c3e4c6748aeaa5ba78c94cd6abe3f5038
Muhammad Hafiz Nugraha <afiznugraha890@gmail.com>
2026-08-31 13:30:10 +0700

    sync config: cors, nginx, docker compose env, gitignore node_modules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    allowedHosts: ['mhfizngrha.my.id', 'www.mhfizngrha.my.id', '43.156.225.37'],
     proxy: {
       '/api': {
         target: 'http://localhost:8787',
